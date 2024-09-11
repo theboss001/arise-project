5b1e72c1cf8aab7a71d43153018b52715a71cd3a
misterJS <landrykolai700@gmail.com>
2024-09-11 05:34:14 +0000

    documentation and code improving

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # arise-project
+
+This JavaScript code defines a Bootstapp class which aims to manage suppliers, modules and exports in a development framework.Here is a detailed analysis of the different parts of the code
+
+1. Imports
+The code begins by importing several classes and modules:
+
+- Guards: 
+    
+
+- Providers
+
+- Exports 
+
+- Modules 
--- bootstrap/Bootstrap.js
+++ bootstrap/Bootstrap.js
@@ -7,55 +7,53 @@ import { UserProductModule } from "../_modules/UserProduct.module.js";
 import { PrintingServiceProvider } from "../_providers/PrintingService.provider.js";
 
 export class Bootstapp {
-  // static suppliers that play role of provider, module, and exports in frameworks
-    static providers = [];
-    static modules = [];
-    static exports = [];
+  // Static arrays to hold providers, modules, and exports
+  static providers = [];
+  static modules = [];
+  static exports = [];
 
-    static _init_() {
-        //triggers all the suppliers
-        let stack = [];
-        [stack = [...this.modules, ...this.providers, ...this.exports]];
-        // console.log(stack.length);
-         for(let k = 0; k < stack.length; k++) {
-          console.log(`starting ${stack[k].name}`, new stack[k]);
-         }
+  // Initializes all the suppliers
+  static _init_() {
+    const stack = [...this.modules, ...this.providers, ...this.exports];
+    for (let k = 0; k < stack.length; k++) {
+      console.log(`Starting ${stack[k].name}`, new stack[k]());
     }
-    /**
-     * @param {Object | Array } set - parameter that provide Objects for binding
-     * @param {Array} context - parameter that provide static suppliers
-     */
-    _sets (set, context) {
-      if(!arguments[0].length && Object.prototype.toString.call(arguments[0]) === "[object Function]") {
-        context.push(set)
-        return context
-      }
-      for( let i = 0; i < set.length; i++) {
-        context.push(set[i])
-      }
-      return context
+  }
+
+  /**
+   * Adds objects to the specified context array.
+   * @param {Object | Array} set - The object(s) to add.
+   * @param {Array} context - The array to which the objects will be added.
+   * @returns {Array} The updated context array.
+   */
+  _sets(set, context) {
+    // If set is not an array, wrap it in an array
+    if (!Array.isArray(set)) {
+      set = [set];
     }
-    constructor (
-      _provide,
-      _export,
-      _module
-    ) {
-      this._sets(_provide, Bootstapp.providers)
-      this._sets(_module, Bootstapp.modules)
-      this._sets(_export, Bootstapp.exports)
-      Bootstapp._init_()
-    }
+    context.push(...set);
+    return context;
+  }
+
+  constructor(_provide, _export, _module) {
+    this._sets(_provide, Bootstapp.providers);
+    this._sets(_module, Bootstapp.modules);
+    this._sets(_export, Bootstapp.exports);
+    Bootstapp._init_();
+  }
 }
+
+// Application instance with specified providers, exports, and modules
 export const app = new Bootstapp(
   [
     ProviderGuard,
     UserProductProvider,
     PrintingServiceProvider
   ],
- [ 
-  ExportGuard,
-  UserModuleExport,
-],
+  [
+    ExportGuard,
+    UserModuleExport,
+  ],
   [
     ModuleGuard,
     UserProductModule
